bdcf34606da1e4c855b209ba0eb17722951479d8
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2022-10-18 16:59:48 +0100

    basic routing

--- Demo/routes/web.php
+++ Demo/routes/web.php
@@ -16,11 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// 
 Route::get('/demo1', function () {
     return view('demo1');
 });
 
-Route::get('/demo2', function () {
-    return view('demo2');
+// shorter ways
+
+Route::view('/demo2', 'demo2');
+
+Route::redirect('/', '/demo2');
+// require parameters
+// capture parameters in route , id for example
+
+Route::get('/user/{id}', function ($id) {
+    return 'user '.$id;
 });
+
+// optional parameters
+// Make sure to give the route's corresponding variable a 
+// default value: in this case $name = 'merradou'
+Route::get('/user/{name?}', function ($name = "merradou") {
+    return $name;
+});
+
+// Regular Expression Constraints
+
+Route::get('/user/{id}', function ($id) {
+    //
+})->where('id', '[0-9]+');
+
+Route::get('/category/{category}', function ($category) {
+    //
+})->whereIn('category', ['movie', 'song', 'painting']);
+
+
+
+
+
+
+
+
